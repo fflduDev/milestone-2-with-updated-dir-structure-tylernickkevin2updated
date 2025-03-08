351e2c7251442c759fe4477fd0e8e8e45e010643
Nicholas Murphy <nicholas.murphy@student.fairfield.edu>
2025-03-07 19:58:57 -0500

    Started phonebook entry

--- template/PhonebookEntry.java
+++ template/PhonebookEntry.java
@@ -9,8 +9,34 @@ package template;
  *   
  */
 
-class PhonebookEntry{
+class PhonebookEntry {
+    private String phoneNumber;
+    private String type;
 
- 
+    public PhonebookEntry(String phoneNumber, String type) {
+        this.phoneNumber = phoneNumber;
+        this.type = type;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(phoneNumber, type);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        PhonebookEntry that = (PhonebookEntry) obj;
+        return Objects.equals(phoneNumber, that.phoneNumber) &&
+                Objects.equals(type, that.type);
+    }
 }
-	
