ffa6396a4e43d30c0f65c6069b0e6fe7edf9df6b
Nicholas Murphy <nicholas.murphy@student.fairfield.edu>
2025-03-07 19:58:20 -0500

    Started contact

--- template/Contact.java
+++ template/Contact.java
@@ -11,6 +11,37 @@ package template;
  */
 
 class Contact {
-	 
+    private String name;
+    private List<PhonebookEntry> phonebookEntries;
 
+    public Contact(String name) {
+        this.name = name;
+        this.phonebookEntries = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<PhonebookEntry> getPhonebookEntries() {
+        return phonebookEntries;
+    }
+
+    public void addPhonebookEntry(String number, String type) {
+        PhonebookEntry pbEntry = new PhonebookEntry(number, type);
+        this.phonebookEntries.add(pbEntry);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name) % 2;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Contact contact = (Contact) obj;
+        return Objects.equals(name, contact.name);
+    }
 }
