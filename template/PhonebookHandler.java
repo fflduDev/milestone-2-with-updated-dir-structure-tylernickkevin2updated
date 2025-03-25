44320f62e6e9f2a22ca31e5d9c2890f28e452675
Tyler Poulin <tyler.poulin@student.fairfield.edu>
2025-03-25 19:04:01 -0400

    updated

--- template/PhonebookHandler.java
+++ template/PhonebookHandler.java
@@ -2,34 +2,59 @@ package template;
 
 import java.util.*;
 
-/**
- * PhonebookHandler - supports 
- * Phonebook operations
- * 
- * Use a map to build the Phonebook
- * key: Contact
- * value: List<phonebookEntries>
- */
+public class PhonebookHandler implements iPhonebookHander {
+    private Map<Contact, List<PhonebookEntry>> phonebook;
 
-public class PhonebookHandler implements iPhonebookHander{
+    public PhonebookHandler(Map<Contact, List<PhonebookEntry>> phonebook) {
+        this.phonebook = phonebook;
+    }
 
-	@Override
-	public List<Contact> sortByName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    // Bubble Sort Implementation
+    @Override
+    public List<Contact> sortByName() {
+        List<Contact> contacts = new ArrayList<>(phonebook.keySet());
 
-	@Override
-	public List<PhonebookEntry> binarySearch(List<Contact> sortedContacts, String name) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        int n = contacts.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (contacts.get(j).getName().compareTo(contacts.get(j + 1).getName()) > 0) {
+                    Collections.swap(contacts, j, j + 1);
+                }
+            }
+        }
+        return contacts;
+    }
 
-	@Override
-	public void display(List<Contact> sortedContacts) {
-		// TODO Auto-generated method stub
-		
-	}
+    // Binary Search Implementation
+    @Override
+    public List<PhonebookEntry> binarySearch(List<Contact> sortedContacts, String name) {
+        int left = 0, right = sortedContacts.size() - 1;
 
- 
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            Contact midContact = sortedContacts.get(mid);
+
+            int comparison = midContact.getName().compareTo(name);
+
+            if (comparison == 0) {
+                return midContact.getPhonebookEntries();
+            } else if (comparison < 0) {
+                left = mid + 1; 
+            } else {
+                right = mid - 1; 
+            }
+        }
+        return Collections.emptyList();
+    }
+
+    // Display all contacts and their entries
+    @Override
+    public void display(List<Contact> sortedContacts) {
+        for (Contact contact : sortedContacts) {
+            System.out.println("Name: " + contact.getName());
+            for (PhonebookEntry entry : contact.getPhonebookEntries()) {
+                System.out.println("  " + entry.getType() + ": " + entry.getPhoneNumber());
+            }
+        }
+    }
 }
--- template/TestHarness.java
+++ template/TestHarness.java
@@ -1,152 +1,121 @@
 package template;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class TestHarness {
 
-	// Contact overrides hashcode && equals.  Observe collision here.
-	private static Map<Contact, List<PhonebookEntry>> phonebook = new HashMap<>();
-	private static Contact p1, p2, p3, p4, p5;
-	private static PhonebookHandler phonebookHander;
+    private static Map<Contact, List<PhonebookEntry>> phonebook = new HashMap<>();
+    private static Contact p1, p2, p3, p4, p5;
+    private static PhonebookHandler phonebookHander;
 
+    public static void main(String[] args) throws InterruptedException {
+        init();
 
-	public static void main(String[] args) throws InterruptedException {
-		init();
-		
-		//TODO: 2 cases:
-		//display non equality using your overriden equals method
-		//display equality using your overriden equals method
+        // Display equality and inequality cases using overridden equals
+        TEST_Equals(p1, p2); 
+        TEST_Equals(p3, p3); 
 
-		TEST_Equals(p1, p2);
-		TEST_Equals(p3, p3);
-		
-		//TODO: 2 cases:
-		//display hashcode case that demonstrates collision
-		//display hashcode case that demonstrates non- collision
+        // Demonstrate hash collisions and non-collisions
+        TEST_hashcode(p1, p3); 
+        TEST_hashcode(p2, p3); 
+        // Create the phonebook handler
+        phonebookHander = new PhonebookHandler(phonebook);
 
-		TEST_hashcode(p1, p3);
-		TEST_hashcode(p2, p3);
+        // Sorting contacts
+        List<Contact> sortedContacts = TEST_Sort(phonebookHander);
+        TEST_Display(sortedContacts);
 
-		//Create the phonebook handler
-		phonebookHander = new PhonebookHandler(phonebook);
-		
-		//TODO
-		//call 
-		List<Contact> sortedContacts = TEST_Sort(phonebookHander);
-		TEST_Display(sortedContacts);
-		
-		//TODO
-		// 2 cases:
-		// 1) a call to search finds the user and displays their entries 
-		//(2) a call to search does not find the user & displays some detail illustrating same
-		TEST_Search(phonebookHander, sortedContacts, "Juan");
-		 
+        // Search test cases
+        TEST_Search(phonebookHander, sortedContacts, "Juan"); // Should find Juan
+        TEST_Search(phonebookHander, sortedContacts, "Alice"); // Should not find Alice
+    }
 
-	}
+    // Test sorting
+    public static List<Contact> TEST_Sort(PhonebookHandler phonebookHandler) {
+        System.out.println("\nSorting Phonebook...");
+        List<Contact> sortedContacts = phonebookHandler.sortByName();
+        return sortedContacts;
+    }
 
-	// 
-	public static List<Contact> TEST_Sort(PhonebookHandler phonebookHandler) {
-		//TODO: call your phonebookHander & have it sort the contacts.  Return those.
-		
-		List<Contact> sortedContacts = phonebookHander.sortByName();
-		return sortedContacts;
-	}
+    // Test searching
+    public static void TEST_Search(PhonebookHandler phonebookHandler, List<Contact> sortedContacts, String name) {
+        System.out.println("\n-- Search results for " + name + " --");
+        List<PhonebookEntry> selectedEntries = phonebookHandler.binarySearch(sortedContacts, name);
 
-	
-	public static void TEST_Search(PhonebookHandler phonebookHandler, List<Contact> sortedContacts, String name) {
-		//TODO: use the sorted contacts and a name to search for.  
-	  
-		
-		System.out.println("\n -- Search results for " + name + " -- ");
-		List<PhonebookEntry> selectedEntries = phonebookHander.binarySearch(sortedContacts, name);
-		for (PhonebookEntry entry : selectedEntries) {
-			
-			System.out.println("Entries for " + name + " " +  entry.getType() + " " + entry.getPhoneNumber());
-		}
-	}
-	
-	public static void TEST_Display(List<Contact> sortedContacts) {
-		phonebookHander.display(sortedContacts);
-	}
+        if (selectedEntries.isEmpty()) {
+            System.out.println("No entries found for " + name);
+        } else {
+            for (PhonebookEntry entry : selectedEntries) {
+                System.out.println("Entries for " + name + " " + entry.getType() + " " + entry.getPhoneNumber());
+            }
+        }
+    }
 
-	public static void TEST_Equals(Contact r1, Contact r2) {
+    // Test displaying contacts
+    public static void TEST_Display(List<Contact> sortedContacts) {
+        System.out.println("\nDisplaying Sorted Phonebook:");
+        phonebookHander.display(sortedContacts);
+    }
 
-		System.out.println("Checking r1.equals(r2) for: \n" + r1.toString() + "\n" + r2.toString());
-		System.out.println(r1.equals(r2));
+    // Test equality of contacts
+    public static void TEST_Equals(Contact r1, Contact r2) {
+        System.out.println("\nChecking r1.equals(r2) for:\n" + r1.getName() + " and " + r2.getName());
+        System.out.println(r1.equals(r2) + "\n");
+    }
 
-		System.out.println();
-	}
+    // Test hashcode for collisions
+    public static void TEST_hashcode(Contact r1, Contact r2) {
+        System.out.println("\nChecking r1.hashcode = r2.hashcode for: \n" + r1.getName() + " and " + r2.getName());
+        if (r1.hashCode() == r2.hashCode()) {
+            System.out.println("Hash matches - Collision detected: " + r1.hashCode());
+        } else {
+            System.out.println("NO hash match: " + r1.hashCode() + " / " + r2.hashCode());
+        }
+        System.out.println();
+    }
 
-	public static void TEST_hashcode(Contact r1, Contact r2) {
+    // Initialize contacts and phonebook entries
+    public static void init() throws InterruptedException {
+        System.out.println("Building Contacts...");
+        TimeUnit.SECONDS.sleep(1);
 
-		System.out.println("Checking r1.hashcode = r2.hashcode for: \n" + r1.toString() + "\n" + r2.toString());
-		if (r1.hashCode() == r2.hashCode()) {
-			System.out.println("Hash matches - we're in the same bucket ( collision!): " + r1.hashCode());
+        p1 = new Contact("Jan");
+        p2 = new Contact("Stan");
+        p3 = new Contact("Juan");
+        p4 = new Contact("Mun");
+        p5 = new Contact("Ran");
 
-		} else {
-			System.out.println("NO hash match: " + r1.hashCode() + " / " + r2.hashCode());
+        System.out.println("Building Phonebook Entries...");
+        TimeUnit.SECONDS.sleep(1);
 
-		}
-		System.out.println();
-	}
- 
+        p1.addPhonebookEntry("2034032233", "Home");
+        p1.addPhonebookEntry("5532233322", "Cell");
+        p1.addPhonebookEntry("1120092829", "Work");
 
-	/** TODO
-	 * 
-	 * Build 5 contacts
-	 * Add 1-3 phonebook entries per contact.   Some must have > 1.
-	 * 'Put' into the phonebook map key-value pairs of the form contact, List<PhonebookEntries>
-	 * 
-	 * Feel free to update 
-	 * 
-	 * @throws InterruptedException
-	 */
-	public static void init() throws InterruptedException {
+        p2.addPhonebookEntry("1034032233", "Home");
+        p2.addPhonebookEntry("2532233322", "Cell");
+        p2.addPhonebookEntry("3120092829", "Work");
 
-		System.out.println("Building Contacts ....");
-		TimeUnit.SECONDS.sleep(1);
+        p3.addPhonebookEntry("4034032233", "Home");
+        p3.addPhonebookEntry("5532233322", "Cell");
+        p3.addPhonebookEntry("6120092829", "Work");
 
-		p1 = new Contact("Jan");
-		p2 = new Contact("Stan");
-		p3 = new Contact("Juan");
-		p4 = new Contact("Mun");
-		p5 = new Contact("Ran");
+        p4.addPhonebookEntry("6034032233", "Home");
+        p4.addPhonebookEntry("3532233322", "Cell");
+        p4.addPhonebookEntry("1120092829", "Work");
 
-		System.out.println("Building Phonebook Entries ....");
-		TimeUnit.SECONDS.sleep(1);
+        p5.addPhonebookEntry("0034032233", "Home");
+        p5.addPhonebookEntry("0532233322", "Cell");
+        p5.addPhonebookEntry("0120092829", "Work");
 
-		p1.addPhonebookEntry("2034032233", "Home");
-		p1.addPhonebookEntry("5532233322", "Cell");
-		p1.addPhonebookEntry("1120092829", "Work");
+        System.out.println("Adding Entries to the phonebook...\n");
+        TimeUnit.SECONDS.sleep(1);
 
-		p2.addPhonebookEntry("1034032233", "Home");
-		p2.addPhonebookEntry("2532233322", "Cell");
-		p2.addPhonebookEntry("3120092829", "Work");
-
-		p3.addPhonebookEntry("4034032233", "Home");
-		p3.addPhonebookEntry("5532233322", "Cell");
-		p3.addPhonebookEntry("6120092829", "Work");
-
-		p4.addPhonebookEntry("6034032233", "Home");
-		p4.addPhonebookEntry("3532233322", "Cell");
-		p4.addPhonebookEntry("1120092829", "Work");
-
-		p5.addPhonebookEntry("0034032233", "Home");
-		p5.addPhonebookEntry("0532233322", "Cell");
-		p5.addPhonebookEntry("0120092829", "Work");
-
-		System.out.println("Adding Entries to the phonebook ....\n");
-		TimeUnit.SECONDS.sleep(1);
-
-		phonebook.put(p1, p1.getPhonebookEntries()); // put ultimately calls overriden hashCode
-		phonebook.put(p2, p2.getPhonebookEntries());
-		phonebook.put(p3, p3.getPhonebookEntries());
-		phonebook.put(p4, p4.getPhonebookEntries());
-		phonebook.put(p5, p5.getPhonebookEntries());
-
-	}
-
+        phonebook.put(p1, p1.getPhonebookEntries());
+        phonebook.put(p2, p2.getPhonebookEntries());
+        phonebook.put(p3, p3.getPhonebookEntries());
+        phonebook.put(p4, p4.getPhonebookEntries());
+        phonebook.put(p5, p5.getPhonebookEntries());
+    }
 }
